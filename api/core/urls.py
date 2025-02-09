8c9626944505f2b167ce67ed320ffde28c46a3cf
alisinasultani255@gmail.com <alisinasultani255@gmail.com>
2025-02-09 11:43:16 +0430

    image reading is solved

--- api/core/urls.py
+++ api/core/urls.py
@@ -1,24 +1,28 @@
-"""
-URL configuration for core project.
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+schema_view = get_schema_view(
+    openapi.Info(
+        title="TET  API Backend ",
+        default_version="v1",
+        description="API endpoints for Authors Haven API Course",
+        contact=openapi.Contact(email="teteam4@gmail.com"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path("", schema_view.with_ui("redoc", cache_timeout=0)),
     path("admin/", admin.site.urls),
     path("api/", include("apps.core.urls")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
